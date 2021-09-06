40f87599cab641e66965b05dd299be2dcb16c754
ssu00 <rachel4535@naver.com>
2021-09-06 17:26:30 +0900

    ⚡️ connected main-login pages

--- components/login/btn/textBtn.jsx
+++ components/login/btn/textBtn.jsx
@@ -1,7 +1,7 @@
 import styles from "./textBtn.module.scss";
-const TextBtn = ({ text }) => {
+const TextBtn = ({ text, onClick }) => {
   return (
-    <button type="button" className={styles.textBtn}>
+    <button type="button" className={styles.textBtn} onClick={onClick}>
       <span className={styles.text}>{text}</span>
     </button>
   );
