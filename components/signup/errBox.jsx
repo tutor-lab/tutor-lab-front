05709c6ae5ce9bba21f1c03de3868c163b98b94d
ssu00 <rachel4535@naver.com>
2021-09-27 20:49:52 +0900

    ⚰️ removed error box

--- components/signup/errBox.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./errBox.module.scss";
-const ErrBox = ({ text }) => {
-  return (
-    <section className={styles.errBox}>
-      <span className={styles.errText}>{text}</span>
-    </section>
-  );
-};
-export default ErrBox;
