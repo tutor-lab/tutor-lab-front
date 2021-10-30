c8d0348294b6959a097ff3d22a4f3f0ecdbe4c5a
ssu00 <rachel4535@naver.com>
2021-10-30 23:09:36 +0900

    ⚡️ added tutor-mypage

---
+++ pages/refundList.jsx
@@ -0,0 +1,50 @@
+import BottomTab from "../components/bottomtab";
+import SearchSection from "../components/mypage/searchSection";
+import OtherTopBar from "../components/mypage/topBar/otherPage";
+import { RefundBox } from "../components/mypage/tuteeBox";
+import styles from "./refundList.module.scss";
+const RefundList = () => {
+  const price = 197000;
+  return (
+    <section className={styles.refundList}>
+      <OtherTopBar title={"환불 목록"} />
+      <SearchSection text1={"전체 환불"} text2={"최근 1년"} />
+      <div className={styles.line} />
+      <section className={styles.refundContent}>
+        <RefundBox
+          date={"2021.07.01"}
+          name={"김민영"}
+          title={"금융권 취업을 위한 데이터 분석 및 모델링 SQL, R, Python"}
+          type={"온라인/그룹"}
+          price={price.toLocaleString("ko-KR")}
+        />
+        <RefundBox
+          date={"2021.07.01"}
+          name={"김민영"}
+          title={"금융권 취업을 위한 데이터 분석 및 모델링 SQL, R, Python"}
+          type={"온라인/그룹"}
+          price={price.toLocaleString("ko-KR")}
+        />
+        <RefundBox
+          date={"2021.07.01"}
+          name={"김민영"}
+          title={"금융권 취업을 위한 데이터 분석 및 모델링 SQL, R, Python"}
+          type={"온라인/그룹"}
+          price={price.toLocaleString("ko-KR")}
+        />
+        <RefundBox
+          date={"2021.07.01"}
+          name={"김민영"}
+          title={"금융권 취업을 위한 데이터 분석 및 모델링 SQL, R, Python"}
+          type={"온라인/그룹"}
+          price={price.toLocaleString("ko-KR")}
+        />
+      </section>
+      <div className={styles.fixed}>
+        <BottomTab />
+      </div>
+    </section>
+  );
+};
+
+export default RefundList;
--- pages/tutorInfoEdit.jsx
+++ pages/tutorInfoEdit.jsx
@@ -1,6 +1,53 @@
 import styles from "./tutorInfoEdit.module.scss";
+import OtherTopBar from "../components/mypage/topBar/otherPage";
+import InputBox from "../components/mypage/inputBox";
+import { SaveBtn } from "../components/mypage/myPageBtn";
 const TutorInfoEdit = () => {
-  return <></>;
+  return (
+    <section className={styles.tutorInfoEdit}>
+      <OtherTopBar title={"튜터 정보 수정"} />
+      <section className={styles.text}>
+        <p className={styles.intro}>
+          입력하신 학력 및 경력 정보를 인증할 수 있는 첨부파일을 추가로 업로드
+          해주세요. 관리자 확인 후, 인증마크를 부여합니다. <br />
+          <span className={styles.caution}>
+            * 첨부파일은 JPEG, PNG, GIF 형식으로만 첨부가능합니다.
+          </span>
+        </p>
+      </section>
+      <div className={styles.line} />
+      <section className={styles.academic}>
+        <div className={styles.firstLine}>
+          <h1 className={styles.title}>학력정보</h1>
+          <label for="academicFile">
+            <div className={styles.fileImg} />
+          </label>
+          <input type="file" id="academicFile" required />
+        </div>
+        <InputBox text={"최종 학력"} order={1} />
+        <InputBox text={"학교 명"} order={2} />
+        <InputBox text={"전공 명"} order={2} />
+        <InputBox text={"그 외 학력"} order={3} />
+      </section>
+      <div className={styles.line} />
+      <section className={styles.career}>
+        <div className={styles.firstLine}>
+          <h1 className={styles.title}>경력정보</h1>
+          <label for="careerFile">
+            <div className={styles.fileImg} />
+          </label>
+          <input type="file" id="careerFile" required />
+        </div>
+        <InputBox text={"직업"} order={1} />
+        <InputBox text={"직장 명"} order={2} />
+        <InputBox text={"그 외 경력"} order={2} />
+        <InputBox text={"자격증"} order={3} />
+      </section>
+      <div className={styles.fixed}>
+        <SaveBtn />
+      </div>
+    </section>
+  );
 };
 
 export default TutorInfoEdit;
