e6675f1de44a67f906a2e4d4dc5ce247a15b4a87
최성준 <csj2702@naver.com>
2021-11-03 23:37:45 +0900

    .

--- redux/reducers/signup.jsx
+++ redux/reducers/signup.jsx
@@ -1,7 +1,7 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
 import createRequestSagaSignUp from "./createRequestSagaSignup";
-import { createRequestActionTypesSignUp } from "./createRequestActionTypesSignUp";
+import  createRequestActionTypesSignUp  from "./createRequestActionTypesSignUp";
 import * as API from "../api/signup";
 import { takeLatest } from "@redux-saga/core/effects";
 
