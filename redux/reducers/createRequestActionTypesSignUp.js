87f4d9ee360ffd1707994bedf93e4d0346d273d6
최성준 <csj2702@naver.com>
2021-11-03 23:32:41 +0900

    redux분리

---
+++ redux/reducers/createRequestActionTypesSignUp.js
@@ -0,0 +1,6 @@
+
+export default function createRequestActionTypesSignUp(type) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+  return [type, SUCCESS, FAILURE];
+};
--- redux/reducers/createRequestSagaSignup.js
+++ redux/reducers/createRequestSagaSignup.js
@@ -1,15 +1,7 @@
 import { call, put } from "redux-saga/effects";
 import { startLoading, finishLoading } from "./loading";
 
-
-export default function createRequestActionTypesSignUp(type) {
-  const SUCCESS = `${type}_SUCCESS`;
-  const FAILURE = `${type}_FAILURE`;
-  return [type, SUCCESS, FAILURE];
-};
-
-
-export function createRequestSagaSignUp(type, request) {
+export default function createRequestSagaSignUp(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
   return function* (action) {
