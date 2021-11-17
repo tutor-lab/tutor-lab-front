bbaa42eff867abea644b22d042b994a03bbb563a
ssu00 <rachel4535@naver.com>
2021-11-17 21:17:25 +0900

    ⚰️ removed dead code

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -4,9 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   ChangeField,
   Initialize,
-  NextStep,
   PrevStep,
-  MoveStep,
   SignUp,
 } from "../redux/reducers/signup";
 import Main from ".";
@@ -112,33 +110,16 @@ const SignUpPage = () => {
     stateBack ? (stateBack.style.display = "block") : "";
   };
 
-  const showGender = () => {
-    const genderBack = document.getElementById("genderBackground");
-    genderBack ? (genderBack.style.display = "block") : "";
-  };
-
   const hideState = () => {
     const stateBack = document.getElementById("stateBackground");
     stateBack ? (stateBack.style.display = "none") : "";
   };
 
-  const hideGender = () => {
-    const genderBack = document.getElementById("genderBackground");
-    genderBack ? (genderBack.style.display = "none") : "";
-  };
-
   const Close = () => {
     const background = document.getElementById("stateBackground");
-    const genderBack = document.getElementById("genderBackground");
     window.addEventListener("click", (e) => {
       e.target === background ? hideState() : false;
-      e.target === genderBack ? hideGender() : false;
     });
-  };
-
-  const Next = (e) => {
-    e.preventDefault();
-    dispatch(NextStep());
   };
 
   const Prev = (e) => {
@@ -161,7 +142,6 @@ const SignUpPage = () => {
           handleChange={onChange}
           handleSubmit={onSubmit}
           showState={showState}
-          showGender={showGender}
           close={Close}
         />
       );
