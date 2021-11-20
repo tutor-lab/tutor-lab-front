b81e952a8c5ae7ac2714264b0bad79c94e5dbe0a
choiseongjun <csj2702@naver.com>
2021-11-21 00:55:34 +0900

    update webpack image

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    loader: "imgix",
     domains: ["tutorlab.s3.ap-northeast-2.amazonaws.com"],
   },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
