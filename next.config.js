61d72fa234700b71df52499f3e011e63bfde8f2c
choiseongjun <csj2702@naver.com>
2021-11-21 00:15:11 +0900

    1.리액트네이티브웹뷰 아이디 던져주기 2.넥스트 빌드시 eslint 꺼짐

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-
-
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["tutorlab.s3.ap-northeast-2.amazonaws.com"],
   },
-}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
