69fb082cdfa7a4659b1f9640ac66e0dc7f8dbdcc
choiseongjun <csj2702@naver.com>
2021-11-21 00:59:10 +0900

    webpack image remove

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    loader: "imgix",
-    domains: ["tutorlab.s3.ap-northeast-2.amazonaws.com"],
-  },
+  // images: {
+  //   loader: "imgix",
+  //   domains: ["tutorlab.s3.ap-northeast-2.amazonaws.com"],
+  // },
 
   eslint: {
     // Warning: This allows production builds to successfully complete even if
