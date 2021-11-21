7e6022b2095de4dae4bc8b5ee8421d06fb9063a6
choiseongjun <csj2702@naver.com>
2021-11-21 10:37:05 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   reactStrictMode: false,
-  // images: {
-  //   loader: "imgix",
-  //   domains: ["tutorlab.s3.ap-northeast-2.amazonaws.com"],
-  // },
+  images: {
+    domains: ["example.com"],
+  },
 
   eslint: {
     // Warning: This allows production builds to successfully complete even if
